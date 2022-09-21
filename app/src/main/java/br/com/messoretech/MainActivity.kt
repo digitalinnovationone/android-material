4b44d3e6e66562b7c95a3d4b08cc8479e015aa71
Ezequiel Messore <ezequiel.messore@hotmail.com>
2022-09-21 14:36:46 -0300

    Adding MainActivity.kt

---
+++ app/src/main/java/br/com/messoretech/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.com.messoretech
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.messoretech.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+}
